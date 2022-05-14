dfc56575e4298ef5b626c2ecf3e93a747078a67c
owentr1369 <tamvaa1306@gmail.com>
2022-05-14 16:25:43 +0700

    fetching api from pokeapi

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -7,8 +7,26 @@
 
 <script>
 // @ is an alias to /src
+import { reactive } from "vue";
 
 export default {
   name: "HomeView",
+  setup() {
+    const state = reactive({
+      pokemons: [],
+      urlIdLookup: {},
+    });
+    fetch("https://pokeapi.co/api/v2/pokemon?limit=100000&offset=0")
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        state.pokemons = data.results;
+        state.urlIdLookup = data.results.reduce(
+          (acc, cur, idx) => (acc = { ...acc, [cur.name]: idx + 1 }),
+
+          {}
+        );
+      });
+  },
 };
 </script>
