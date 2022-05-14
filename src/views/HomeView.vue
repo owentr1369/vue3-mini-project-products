0fcf5bf30a01109a4f0830231c127bb7b25fa5bd
owentr1369 <tamvaa1306@gmail.com>
2022-05-14 16:52:24 +0700

    filtering pokemon by input value

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,13 +1,29 @@
 <template>
-  <div class="home">
+  <div class="w-full flex justify-center">
     <img alt="Vue logo" src="../assets/logo.png" />
-    <h1>Home</h1>
+  </div>
+  <div class="w-full flex justify-center">
+    <input
+      type="text"
+      placeholder="Enter Pokemon here"
+      class="mt-2 p-2 border-orange-500 border-2"
+      v-model="text"
+    />
+  </div>
+  <div class="mt-10 p-4 flex flex-wrap justify-center">
+    <div
+      v-for="(pokemon, idx) in filteredPokemons"
+      :key="idx"
+      class="ml-4 text-2x text-blue-200"
+    >
+      {{ pokemon.name }}
+    </div>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import { reactive } from "vue";
+import { computed, reactive, toRefs } from "vue";
 
 export default {
   name: "HomeView",
@@ -15,7 +31,17 @@ export default {
     const state = reactive({
       pokemons: [],
       urlIdLookup: {},
+      text: "",
+      filteredPokemons: computed(() => updatePokemon()),
     });
+    function updatePokemon() {
+      if (!state.text) {
+        return [];
+      }
+      return state.pokemons.filter((pokemon) =>
+        pokemon.name.includes(state.text)
+      );
+    }
     fetch("https://pokeapi.co/api/v2/pokemon?limit=100000&offset=0")
       .then((res) => res.json())
       .then((data) => {
@@ -27,6 +53,7 @@ export default {
           {}
         );
       });
+    return { ...toRefs(state) };
   },
 };
 </script>
